c46da1413df0de109ba8a74d35fdd83787be3e3f
Starkda <starkda@bogon>
2017-07-31 01:02:24 +0800

    ###19-美团外卖视图高度-视图高度变化的范围 - 在更新之前判断,二者之和是否超出了范围,如果超出则设置为临界值 - 快速拖拽时的判断问题? - 在判断中设置到指定位置'shopHeaderView的高-最矮64,最高180',细节处理
    ```
    //
    CGFloat newShopHeaderViewH = _shopHeaderView.bounds.size.height + p.y;
    
    [_shopHeaderView mas_updateConstraints:^(MASConstraintMaker *make) {
    
        if (newShopHeaderViewH >= 180) {
            make.height.offset(180);
        } else  if (newShopHeaderViewH <= 64) {
            make.height.offset(64);
        } else {
            make.height.offset(newShopHeaderViewH);
        }
    
    }];
    ```

--- KDMeiTuan/Classes/Shop/KDShopController.m
+++ KDMeiTuan/Classes/Shop/KDShopController.m
@@ -56,7 +56,7 @@
     
     [shopHeaderview mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.top.right.offset(0);
-        make.height.offset(100);
+        make.height.offset(180);
     }];
     
     _shopHeaderView = shopHeaderview;
@@ -75,9 +75,29 @@
     //获得平移的距离
     CGPoint p = [pan translationInView:pan.view];
     
+    //头部视图的高度
+    CGFloat shopHeaderViewUpdateHeight = _shopHeaderView.bounds.size.height;
+    
     [_shopHeaderView mas_updateConstraints:^(MASConstraintMaker *make) {
         make.height.offset(p.y + _shopHeaderView.bounds.size.height);
+        
+    //如果它当时的高度加上平移的值 <= 64 直接就让他变成到64
+    if(p.y + shopHeaderViewUpdateHeight < 64){
+    
+        make.height.offset(64);
+        
+        //如果他当前的高度加上平移的值 >= 180 直接让它变成180
+    }else if(p.y + shopHeaderViewUpdateHeight >= 180){
+        
+        make.height.offset(180);
+    }else{
+        //在大于64及小于180之间让它慢慢变化
+        make.height.offset(p.y + shopHeaderViewUpdateHeight);
+    }
+    
+        
     }];
+
     
     
     //恢复到初始值
