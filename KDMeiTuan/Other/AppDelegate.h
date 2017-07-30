9a497fad012bce1f3d88a1b91164542ed6c4966a
Starkda <starkda@StarkdadeMacBook-Pro.local>
2017-07-30 19:45:40 +0800

    准备工作

--- KDMeiTuan/Other/AppDelegate.h
+++ KDMeiTuan/Other/AppDelegate.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  AppDelegate.h
 //  KDMeiTuan
 //
 //  Created by Starkda  on 2017/7/30.
@@ -8,7 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
 
 
 @end
